03fcbc14e5f07206495f6034db59f6da1feb88ec
maverfrick <mr.thai2802@gmail.com>
2025-12-12 18:33:05 -0500

    graph finalize

--- group4final/src/components/bar_chart.js
+++ group4final/src/components/bar_chart.js
@@ -1,3 +1,11 @@
-d3.csv("/data/amsterdam_weekdays.csv").then((data) => {
-    // use data here
-});
+import * as d3 from 'd3';
+
+function BarChart() {
+    return (
+        <div>
+            <h1>Bar Chart</h1>
+        </div>
+    );
+}
+
+export default BarChart;
--- group4final/src/components/scatter_plot.js
+++ group4final/src/components/scatter_plot.js
@@ -1,3 +1,11 @@
-d3.csv("/data/amsterdam_weekdays.csv").then((data) => {
-    // use data here
-});
+import * as d3 from 'd3';
+
+function ScatterPlot() {
+    return (
+        <div>
+            <h1>Scatter Plot</h1>
+        </div>
+    );
+}
+
+export default ScatterPlot;
